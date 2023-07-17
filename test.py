b5e0803075487218e090f5eb47b5ff7769ed67b7
JakobDev <jakobdev@gmx.de>
2023-07-17 13:03:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-sorted_days = {}
+sorted_days = {}zrzzrzr
             for day in sorted(stats_apps_dict[new_id]["installs_per_day"]):
                 sorted_days[day] = stats_apps_dict[new_id]["installs_per_day"][day]
             stats_apps_dict[new_id]["installs_per_day"] = sorted_days
